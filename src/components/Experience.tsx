aafc836ec01951e1af60cb0fde31612a68e4a4fe
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-11-17 22:51:45 +0700

    feat(home): add project section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -5,11 +5,11 @@ export default function Experience() {
   return (
     <SectionWrapper>
       <SectionTitle title="Experience" />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4">
         {Array.from({ length: 3 }).map((_, i) => (
           <div
             key={i}
-            className="flex items-start gap-x-4 border border-neutral-300 rounded-lg p-4"
+            className="flex items-start gap-x-4 border border-neutral-300 shadow rounded-lg p-4"
           >
             <Image
               src="/exp/exp1.webp"

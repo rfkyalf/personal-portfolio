66ba01779cc935c742c35223e5ff7265e8dd9920
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-11-21 10:33:26 +0700

    feat(api): add some data to certificates api

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -16,7 +16,7 @@ export default async function Certificates() {
             href={cert.url}
             target="_blank"
             key={cert.id}
-            className="flex flex-col border border-neutral-300 shadow rounded-lg p-2 md:p-4"
+            className="flex flex-col justify-between border border-neutral-300 shadow rounded-lg p-2 md:p-4"
           >
             <div className="flex items-center gap-x-2">
               {cert.skills.map((skill) => {
@@ -46,7 +46,7 @@ export default async function Certificates() {
               alt={cert.title}
               width={100}
               height={100}
-              className="w-full h-full object-cover rounded-md mt-2"
+              className="w-full h-[220px] object-cover rounded-md mt-2"
             />
             <p className="self-end text-[0.8rem] md:text-[0.9rem] text-neutral-900 mt-2 md:mt-3 lg:mt-4">
               {cert.date}

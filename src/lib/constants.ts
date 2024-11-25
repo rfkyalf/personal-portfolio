022925522946dfaec5b3fd39cdd362ee61a4866d
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-11-25 17:11:49 +0700

    add domain

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,7 @@
 export const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+  process.env.NEXT_PUBLIC_BASE_URL ||
+  'http://localhost:3000' ||
+  'https://rifkyalfarez.my.id';
 
 export const NAV_LIST = [
   {

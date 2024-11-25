5359ebe98d44414d773d3b910845bf3dd5c5a64a
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-11-25 10:58:07 +0700

    fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,17 +3,31 @@ import { BASE_URL } from '@/lib/constants';
 import { MetadataRoute } from 'next';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const projects = await getProjects('');
-  const projectsUrl = projects.data.map((project: { slug: string }) => ({
+  let projects = [];
+  let experiences = [];
+
+  try {
+    const projectRes = await getProjects('');
+    projects = projectRes?.data || [];
+  } catch (error) {
+    console.error('Error fetching projects:', error);
+  }
+
+  try {
+    const experienceRes = await getExperiences();
+    experiences = experienceRes?.data || [];
+  } catch (error) {
+    console.error('Error fetching experiences:', error);
+  }
+
+  const projectsUrl = projects.map((project: { slug: string }) => ({
     url: `${BASE_URL}/projects/${project.slug}`,
   }));
 
-  const experiences = await getExperiences();
-  const experiencesUrl = experiences.data.map(
-    (experience: { slug: string }) => ({
-      url: `${BASE_URL}/experiences/${experience.slug}`,
-    })
-  );
+  const experiencesUrl = experiences.map((experience: { slug: string }) => ({
+    url: `${BASE_URL}/experiences/${experience.slug}`,
+  }));
+
   return [
     {
       url: `${BASE_URL}`,
@@ -37,3 +51,39 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     ...experiencesUrl,
   ];
 }
+
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+//   const projects = await getProjects('');
+//   const projectsUrl = projects.data.map((project: { slug: string }) => ({
+//     url: `${BASE_URL}/projects/${project.slug}`,
+//   }));
+
+//   const experiences = await getExperiences();
+//   const experiencesUrl = experiences.data.map(
+//     (experience: { slug: string }) => ({
+//       url: `${BASE_URL}/experiences/${experience.slug}`,
+//     })
+//   );
+//   return [
+//     {
+//       url: `${BASE_URL}`,
+//       lastModified: new Date(),
+//       changeFrequency: 'yearly',
+//       priority: 1,
+//     },
+//     {
+//       url: `${BASE_URL}/projects`,
+//       lastModified: new Date(),
+//       changeFrequency: 'monthly',
+//       priority: 0.8,
+//     },
+//     {
+//       url: `${BASE_URL}/certificates`,
+//       lastModified: new Date(),
+//       changeFrequency: 'yearly',
+//       priority: 0.7,
+//     },
+//     ...projectsUrl,
+//     ...experiencesUrl,
+//   ];
+// }

f5f80a3d8b1439745313e192c206fec29cf40759
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-11-25 11:09:52 +0700

    update base url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,1 @@
-export const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL ||
-  process.env.NEXT_PUBLIC_BASE_URL_VERCEL ||
-  process.env.NEXT_PUBLIC_BASE_URL_MY_ID;
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;

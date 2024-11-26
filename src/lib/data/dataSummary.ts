578665d83d12cde41476c2f9cc1f3dc4c0d8b26d
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-11-26 12:54:43 +0700

    icons optimized

--- src/lib/data/dataSummary.ts
+++ src/lib/data/dataSummary.ts
@@ -3,5 +3,6 @@ export const dataSummary = {
   role: 'Frontend Developer',
   summary:
     'Graduated from Informatics of Siliwangi University with a deep interest in web development. Skilled and experienced in designing, developing, and testing websites using modern technologies such as Next.js and React.js.',
-  cv_link: '/CV_RifkyAlfarez.pdf',
+  cv_link:
+    'https://drive.google.com/file/d/1HpCOQffakJ0pCHiYcPJ7vMpCVmKshTQU/view?usp=sharing',
 };

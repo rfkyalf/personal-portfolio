b19e3f116a77a0527306899c0fda3e59b7004e17
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-11-27 13:42:39 +0700

    edit base url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const BASE_URL =
   process.env.NEXT_PUBLIC_BASE_URL ||
-  'http://localhost:3000' ||
-  'https://rifkyalfarez.my.id';
+  'https://rifkyalfarez.vercel.app' ||
+  'http://localhost:3000';
 
 export const NAV_LIST = [
   {

0808e15ed302eeceb9193985fe84e0f89f1783e4
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-11-30 08:58:36 +0700

    feat(app): add 96x96 favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico', type: 'image/x-icon' },
+      { url: '/favicon-96x96.png', type: 'image/png', sizes: '96x96' },
       { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
       { url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
       { url: '/favicon.svg', type: 'image/svg+xml' },

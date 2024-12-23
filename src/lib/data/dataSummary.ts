5b0bfec723477122db0f1701a589c1734e968fdb
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-12-23 07:46:17 +0700

    feat(summary): update summary and cv link

--- src/lib/data/dataSummary.ts
+++ src/lib/data/dataSummary.ts
@@ -2,7 +2,7 @@ export const dataSummary = {
   name: 'Rifky Alfarez',
   role: 'Frontend Developer',
   summary:
-    'An enthusiastic and motivated Informatics Engineering graduate with a strong passion for web development. Skilled in developing websites using modern technologies such as Next.js and React.js. Supported by hands-on experience from an internship as a Frontend Developer and various web development projects.',
+    'An enthusiastic and motivated Informatics Engineering graduate with a strong passion for web development. Skilled in developing websites using modern technologies such as Vue.js, React.js, and Next.js. Supported by hands-on experience from an internship as a Frontend Developer and various web development projects.',
   cv_link:
-    'https://drive.google.com/file/d/1fPaRn51LuSwKnn1ohr8CQrEYSuFG03ou/view?usp=drive_link',
+    'https://drive.google.com/file/d/1HE9rEIjQWAgj8eFe6aeFIAupgVw81J8b/view?usp=drive_link',
 };
